5b7a27bbc62930d4de30a19f2958a95639374cc8
gquantym <151845051+gquantym@users.noreply.github.com>
2023-12-12 12:14:29 +0000

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
-def calculate_specificity_sensitivity(conf_matrix):
+'''def calculate_specificity_sensitivity(conf_matrix):
     true_negatives = conf_matrix[0, 0]
     false_positives = conf_matrix[0, 1]
     false_negatives = conf_matrix[1, 0]
@@ -15,27 +15,27 @@ def calculate_specificity_sensitivity(conf_matrix):
     # Sensitivity calculation
     total_actual_positives = true_positives + false_negatives
     sensitivity = true_positives / total_actual_positives
+    
+    return specificity, sensitivity'''
 
-    return specificity, sensitivity
 
-def find_optimal_threshold(probabilities, true_labels):
-    thresholds = np.linspace(0, 1, 100)
+def find_optimal_threshold(probabilities, true_labels, threshold):
     specificities = []
     sensitivities = []
 
-    for threshold in thresholds:
-        predicted_labels = (probabilities >= threshold).astype(int)
-        cm = confusion_matrix(true_labels, predicted_labels)
-        specificity, sensitivity = calculate_specificity_sensitivity(cm)
-        specificities.append(specificity)
-        sensitivities.append(sensitivity)
 
-    optimal_threshold_index = np.argmax(specificities)
-    optimal_threshold = thresholds[optimal_threshold_index]
-    max_specificity = specificities[optimal_threshold_index]
-    corresponding_sensitivity = sensitivities[optimal_threshold_index]
+    predicted_labels = (probabilities >= threshold).astype(int)
+    cm = confusion_matrix(true_labels, predicted_labels)
+    specificity, sensitivity = calculate_specificity_sensitivity(cm)[:2]
+    
+    specificities.append(specificity)
+    sensitivities.append(sensitivity)
 
-    return optimal_threshold, max_specificity, corresponding_sensitivity, thresholds, specificities, sensitivities
+
+    max_specificity = specificities[threshold]
+    corresponding_sensitivity = sensitivities[threshold]
+
+    return max_specificity, corresponding_sensitivity,specificities,sensitivities
 
 def plot_specificity_sensitivity_vs_threshold(thresholds, specificities, sensitivities, optimal_threshold, max_specificity, corresponding_sensitivity):
     plt.plot(thresholds, specificities, label='Specificity')
@@ -49,6 +49,41 @@ def plot_specificity_sensitivity_vs_threshold(thresholds, specificities, sensiti
     plt.show()
 
 
+'''----------------------------------------------------------------------------------------------------------------------'''
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
+def calculate_specificity_sensitivity(true_labels, predicted_probabilities, threshold):
+    predicted_labels = (predicted_probabilities >= threshold).astype(int)
+    cm = confusion_matrix(true_labels, predicted_labels)
+
+    true_negatives = cm[0, 0]
+    false_positives = cm[0, 1]
+    false_negatives = cm[1, 0]
+    true_positives = cm[1, 1]
+
+    # Specificity calculation
+    total_actual_negatives = true_negatives + false_positives
+    specificity = true_negatives / total_actual_negatives
+
+    # Sensitivity calculation
+    total_actual_positives = true_positives + false_negatives
+    sensitivity = true_positives / total_actual_positives
+    
+    
+    print("true_negatives == ", true_negatives)
+    print("true_positives == ", true_positives)
+    print("total_actual_negatives == ", total_actual_negatives)
+    print("total_actual_positives == ", total_actual_positives)
+    print("len(true_labels) == ",len(true_labels))
+    
+    print("--------------------------------")
+    acc = accurary(true_positives,true_negatives, len(true_labels))
+    
+    
+    return specificity, sensitivity, acc 
+
+def accurary(TP,TN,data_length):
+    return (TP+TN)/data_length
 
 
